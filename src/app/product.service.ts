085a944f474631e36c2e0594ba6b77e6682667af
vishal saxena <vsaxena115@gmail.com>
2019-01-25 02:44:50 +0530

    test

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,8 +5,10 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) { }
   private _albumUrl : '../assets/album.json';
+  
+  constructor(private _http: Http) { }
+  
   getAlbum(id: number){
     return this._http.get(this._albumUrl).map((response)=>response.json()); 
   }
